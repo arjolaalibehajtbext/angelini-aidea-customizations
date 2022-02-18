1200d8b7ffcd17820919eff4e3aa4feef312554a
Arjola Alibehaj <arjola.alibehaj.ext@truebluecorp.com>
2022-02-18 12:22:18 +0100

    empty "External privacy source" and "Date external privacy" of contact when creating a new privacy consent

---
+++ TrueBlue.Aidea.Plugin.AngCp.Customizations/PrivacyConsent/AideaAngCpCustomPrivacyConsentPlugin.cs
@@ -0,0 +1,16 @@
+﻿using TrueBlue.Aidea.Plugin.AngCp.Customization.Archive.PrivacyConsent.BusinessLogic;
+using TrueBlue.Aidea.Plugin.AngCp.Customization.Common.Base;
+
+namespace TrueBlue.Aidea.Plugin.AngCp.Customization.Archive.PrivacyConsent
+{
+    public class AideaAngCpCustomPrivacyConsentPlugin : PluginBase
+    {
+        public AideaAngCpCustomPrivacyConsentPlugin() : base("AideaAngCpCustomPrivacyConsentPlugin") { }
+
+        public override void ExecutePlugin()
+        {
+            AideaAngCpCustomPrivacyConsentBusinessLogic bl = new AideaAngCpCustomPrivacyConsentBusinessLogic(this);
+            bl.ApplyLogic();
+        }
+    }
+}
---
+++ TrueBlue.Aidea.Plugin.AngCp.Customizations/PrivacyConsent/BusinessLogic/AideaAngCpCustomPrivacyConsentBusinessLogic.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using TrueBlue.Aidea.Plugin.AngCp.Customization.Common.Base;
+using TrueBlue.Aidea.Plugin.AngCp.Customization.Common.Model;
+
+namespace TrueBlue.Aidea.Plugin.AngCp.Customization.Archive.PrivacyConsent.BusinessLogic
+{
+    public class AideaAngCpCustomPrivacyConsentBusinessLogic : BusinessLogicBase
+    {
+        #region Ctor
+
+        public AideaAngCpCustomPrivacyConsentBusinessLogic(PluginBase plugin) : base(plugin) { }
+
+        #endregion
+
+        #region Public Methods
+
+        public override void ApplyLogic()
+        {
+            if (MessageName == ContextMessageName.Create)
+                ApplyCreateLogic();
+            else if (MessageName == ContextMessageName.Update)
+                ApplyUpdateLogic();
+        }
+
+        #endregion
+
+        #region Create Logic
+
+        private void ApplyCreateLogic()
+        {
+            TraceLog("Apply create business logic");
+            Entity target = GetTargetEntity();
+            EntityReference hcpER;
+
+            if (!target.Contains("tb_hcp"))
+                throw new Exception(RetrieveLocalizedString("error_NoContact"));
+            else 
+                hcpER =  target.GetAttributeValue<EntityReference>("tb_hcp");
+
+            if (hcpER != null)
+            {
+                Entity contact = new Entity("contact");
+                contact["contactid"] = hcpER.Id;
+                contact["tbc_externalprivacydatetime"] = null;
+                contact["tbc_externalprivacysource"] = null;
+                Service.Update(contact);
+            }
+        }
+
+        #endregion
+
+        #region Update Logic
+
+        public void ApplyUpdateLogic()
+        {
+        }
+
+        #endregion
+
+        #region Common
+        #endregion
+    }
+}
